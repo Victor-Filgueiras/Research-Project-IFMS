4278264fc843097db4d46618db5d5e8b530cbd58
Victor-Filgueiras <dev.filgueiras@gmail.com>
2025-03-12 20:01:50 -0300

    Update carRegister.py

--- carRegister.py
+++ carRegister.py
@@ -1,12 +1,52 @@
 from tkinter import *
+import psycopg2 
 
 def sendInfo():
     name = driverName.get()
     plate = plateCode.get()
     user_type = personOption.get()
+
     print(f'O motorista se chama: {name}')
     print(f'A placa do carro é: {plate}')
     print(f'Tipo de usuário: {user_type}')
+
+    connect = psycopg2.connect(
+        dbname="sistemacadastro",
+        user="postgres",
+        host="localhost",
+        password="945d7a2d195b46afbb322b2e94e10846",
+        port="5432"
+    )
+
+    cursor = connect.cursor()
+
+    if user_type == 'Estudante':
+        query = f"""
+            INSERT INTO Estudantes(fullName, plateCode)
+            VALUES ('{name}', '{plate}');
+        """
+        cursor.execute(query)
+
+    if user_type == 'Servidor':
+        query = f"""
+            INSERT INTO Servidores(fullName, plateCode)
+            VALUES ('{name}', '{plate}');
+        """
+        cursor.execute(query)
+
+    if user_type == 'Visitante':
+        query = f"""
+            INSERT INTO Visitantes(fullName, plateCode)
+            VALUES ('{name}', '{plate}');
+        """
+        cursor.execute(query)
+
+    connect.commit()
+    connect.close()
+
+    driverName.delete(0, END)
+    plateCode.delete(0, END)
+    personOption.set('')
 
 window = Tk()
 window.geometry('400x300')
