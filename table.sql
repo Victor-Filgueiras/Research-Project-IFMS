910a87e1167ca90738e27b693f424e77b96f2b84
Victor-Filgueiras <dev.filgueiras@gmail.com>
2025-03-12 20:01:00 -0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,17 +1,17 @@
 CREATE TABLE Servidores (
     fullName CHAR(100) NOT NULL,
-    plate VARCHAR(7) NOT NULL,
-    PRIMARY KEY (fullName, plate)
+    plateCode VARCHAR(7) NOT NULL,
+    PRIMARY KEY (fullName, plateCode)
 );
 
 CREATE TABLE Estudantes (
     fullName CHAR(100) NOT NULL,
-    plate VARCHAR(7) NOT NULL,
-    PRIMARY KEY (fullName, plate)
+    plateCode VARCHAR(7) NOT NULL,
+    PRIMARY KEY (fullName, plateCode)
 );
 
 CREATE TABLE Visitantes (
     fullName CHAR(100) NOT NULL,
-    plate VARCHAR(7) NOT NULL,
-    PRIMARY KEY (fullName, plate)
+    plateCode VARCHAR(7) NOT NULL,
+    PRIMARY KEY (fullName, plateCode)
 );
